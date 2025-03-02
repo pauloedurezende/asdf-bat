6ffd67d1d471843d564b762fbeda5930b9b0bf31
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2025-03-01 22:42:16 -0300

    refactor: clean up comments and improve version listing logic

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for bat.
 GH_REPO="https://github.com/sharkdp/bat"
 TOOL_NAME="bat"
 TOOL_TEST="bat --version"
@@ -14,7 +13,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if bat is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -26,13 +24,11 @@ sort_versions() {
 
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		grep -o 'refs/tags/v.*' | cut -d/ -f3- |
+		sed 's/^v//'
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if bat has other means of determining installable versions.
 	list_github_tags
 }
 
